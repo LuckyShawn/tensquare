d8365b102e25c44a49d66ea963a0dbee1646bce7
LuckyShawn <LuckyShawn@foxmail.com>
2019-02-12 16:53:58 +0800

    增加问答微服务，招聘微服务

--- tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
+++ tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -3,14 +3,13 @@ package com.tensquare.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import utils.IdWorker;
+import util.IdWorker;
 
 @SpringBootApplication
-public class TensquareBaseApplication {
+public class BaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TensquareBaseApplication.class, args);
+        SpringApplication.run(BaseApplication.class, args);
     }
 
     @Bean
