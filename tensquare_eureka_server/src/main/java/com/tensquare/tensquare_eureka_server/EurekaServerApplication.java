ea257cc4302680c1e672f7d3aa8ff5ed7e49c6a4
luckyshawn <474240165@qq.com>
2019-02-21 23:16:10 +0800

    导入了包

--- tensquare_eureka_server/src/main/java/com/tensquare/tensquare_eureka_server/EurekaServerApplication.java
+++ tensquare_eureka_server/src/main/java/com/tensquare/tensquare_eureka_server/EurekaServerApplication.java
@@ -2,6 +2,7 @@ package com.tensquare.tensquare_eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
